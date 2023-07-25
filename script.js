e0894db13c4b545201f469bb9e45a6f1e2f29175
Nouamanebld <albert.angel14@gmail.com>
2023-07-25 09:07:57 +0100

    Create ships factory function that return an array of ships coordinates

--- script.js
+++ script.js
@@ -11,4 +11,18 @@ const ship = (length) => {
   return { length, hits, hit, isSunk };
 };
 
-module.exports = ship;
+const ships = (length, direction, location) => {
+  const shipsArr = [];
+  if (direction === 'h') {
+    for (let i = 0; i < length; i++) {
+      shipsArr.push(location + i);
+    }
+  } else if (direction === 'v') {
+    for (let i = 0; i < length t* 10; i += 10) {
+      shipsArr.push(location + i);
+    }
+  }
+  return shipsArr;
+};
+
+module.exports = { ship, ships };
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const ship = require('./script');
+const { ship, ships } = require('./script');
 
 describe('Sunking', () => {
   test('isSunk should return false', async () => {
@@ -7,9 +7,24 @@ describe('Sunking', () => {
   });
 });
 
-describe('hit', () => {
+describe('Hit', () => {
   test('hit should be equal to 1', async () => {
     const result = await ship(2).hit();
     expect(result).toBe(1);
   });
+});
+
+test('ships should return [0, 1]', async () => {
+  const result = await ships(2, 'h', 0);
+  expect(result).toEqual([0, 1]);
+});
+
+test('ships should return [3, 4, 5]', async () => {
+  const result = await ships(3, 'h', 3);
+  expect(result).toEqual([3, 4, 5]);
+});
+
+test('ships should return [0, 1]', async () => {
+  const result = await ships(2, 'v', 0);
+  expect(result).toEqual([0, 10]);
 });
